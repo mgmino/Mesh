a7223122df96b4c9fd35c3ece12adbec54f9aa48
bmino <brandonmino@bellsouth.net>
2016-12-27 01:00:38 -0500

    Linked-In Directive
    Generates linked in logo and href given
    - Contact first name (may include middle also)
    - Contact last name

--- public/directives/directives.js
+++ public/directives/directives.js
@@ -1,30 +1,26 @@
 angular.module('directives', [])
 
-.directive('linkedIn', function() {
+.directive('linkedIn', [function() {
     return {
+        restrict: 'E',
+        template: '<a ng-href="{{linkedInProfile}}" target="_blank"><img src="pix/icons/linkedin.gif"/></a>',
         scope: {
             fname: '=',
             lname: '='
         },
         link: function (scope, elem, attrs) {
 
-            console.log(scope.fname);
+            // Makes a guess
+            // Will remain if neither a first nor last name is provided
+            scope.linkedInProfile = 'https://www.linkedin.com/vsearch/f?keywords=' + scope.fname + ' ' + scope.lname + '&search=Search';
 
-            // var parsedFirstName = scope.fname.substr(0, scope.fname.indexOf(" "));
-            // console.log(parsedFirstName);
-            //
-            // var img_element = angular.element('<img/>');
-            // img_element.attr('src', 'pix/icons/linkedin.gif');
-            //
-            // // Builds <a> tag
-            // var a_element = angular.element('<a/>');
-            // a_element.attr('href', 'https://www.linkedin.com/vsearch/f?keywords=' + parsedFirstName + scope.lname + '&search=Search');
-            // a_element.attr('target', '_blank');
-            //
-            // // Adds LinkedIn logo to <a> tag
-            // a_element.append(img_element);
-            //
-            // elem.append('hey');
+            scope.$watchGroup(['fname', 'lname'], function(newVals, oldVals) {
+                if (!newVals[0] || !newVals[1]) return;
+                var name = angular.copy(scope.fname);
+                var space_index = name.indexOf(' ');
+                var firstName = space_index == -1 ? name : name.substr(0, space_index);
+                scope.linkedInProfile = 'https://www.linkedin.com/vsearch/f?keywords=' + firstName + ' ' + scope.lname + '&search=Search';
+            });
         }
     }
-});
+}]);
