2a9c410f1688621356a465c70b34d2f1e7ebca11
bmino <brandonmino@bellsouth.net>
2017-01-02 14:56:57 -0500

    Implementing New Directives
    pinfo
    contactImage
    contactTags

--- public/directives/directives.js
+++ public/directives/directives.js
@@ -3,24 +3,18 @@ angular.module('directives', [])
 .directive('linkedIn', [function() {
     return {
         restrict: 'E',
-        template: '<a ng-href="{{linkedInProfile}}" target="_blank"><img src="pix/icons/linkedin.gif"/></a>',
+        templateUrl: 'directives/partials/linkedIn.html',
         scope: {
             fname: '=',
             lname: '='
         },
         link: function (scope, elem, attrs) {
-
-            // Makes a guess
-            // Will remain if neither a first nor last name is provided
-            scope.linkedInProfile = 'https://www.linkedin.com/vsearch/f?keywords=' + scope.fname + ' ' + scope.lname + '&search=Search';
-
-            scope.$watchGroup(['fname', 'lname'], function(newVals, oldVals) {
-                if (!newVals[0] || !newVals[1]) return;
+            function firstName() {
                 var name = angular.copy(scope.fname);
                 var space_index = name.indexOf(' ');
-                var firstName = space_index == -1 ? name : name.substr(0, space_index);
-                scope.linkedInProfile = 'https://www.linkedin.com/vsearch/f?keywords=' + firstName + ' ' + scope.lname + '&search=Search';
-            });
+                return space_index == -1 ? name : name.substr(0, space_index);
+            }
+            scope.linkedInProfile = 'https://www.linkedin.com/vsearch/f?keywords=' + firstName() + ' ' + scope.lname + '&search=Search';
         }
     }
 }])
@@ -29,11 +23,7 @@ angular.module('directives', [])
 .directive('alertBox', ['alertService', function (alertService) {
     return {
         restrict: 'E',
-        template: '<div ng-repeat="alert in getAlerts()" class="alert alert-{{alert.type}} alert-dismissible">'+
-                        '<button type="button" class="close" ng-click="removeAlert(alert)" aria-hidden="true"><i class="fa fa-close"></i></button>'+
-                        '<div class="alert-body"><i class="icon fa fa-{{getAlertIcon(alert)}}"></i> {{alert.msg}}</div>'+
-                    '</div>',
-
+        templateUrl: 'directives/partials/alertBox.html',
         link: function(scope, elems, attrs) {
             scope.getAlerts = function() {
                 return alertService.getAlerts();
@@ -54,6 +44,37 @@ angular.module('directives', [])
                         break;
                 }
             }
+        }
+    }
+}])
+
+.directive('pinfo', [function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/partials/pinfo.html',
+        scope: {
+            details: '='
+        }
+    }
+}])
+
+.directive('contactImage', [function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/partials/contactImage.html',
+        scope: {
+            contact: '='
+        }
+    }
+}])
+
+
+.directive('contactTags', [function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/partials/contactTags.html',
+        scope: {
+            contact: '='
         }
     }
 }]);
