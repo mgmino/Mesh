1fe6b3ff860632e4a67a6ac09f93149214fec8a5
bmino <brandonmino@bellsouth.net>
2017-01-02 23:39:44 -0500

    Removing Node.js
    Also includes:
    
    Removing footer.
    Linking to details page is now through contact photo.
    Removing unused font icon libraries.
    - Only bootstrap (glyphicons) and font-awesome (fa) remain

--- routes/utils.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET filename of contact picture */
-router.get('/getPhotoName', function(req, res, next) {
-  res.render('index');
-});
