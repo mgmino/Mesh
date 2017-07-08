3cc8e3f8574aa7017e1fe2ab8d11aa308866663f
mgmino <mmino@sabaitechnology.com>
2017-07-08 15:26:27 -0400

    ccs styling, menu reorg

--- services/queryService.js
+++ services/queryService.js
@@ -1,11 +1,11 @@
-angular.module('services')
-.service('queryService', ['$http', '$location', 'API_URL', function($http, $location, API_URL) {
+angular.module("services")
+.service("queryService", ['$http', '$location', 'API_URL', function($http, $location, API_URL) {
 
     $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
     this.getAllContacts = function() {
         return $http({
-            method : 'POST',
+            method : "POST",
             url : API_URL,
             data: {
                 fld:    'lname',
@@ -17,7 +17,7 @@ angular.module('services')
 
     this.getFavorites = function() {
         return $http({
-            method : 'POST',
+            method : "POST",
             url : API_URL,
             data: {
                 fld:    'tags',
@@ -27,9 +27,9 @@ angular.module('services')
         }).then(processSuccess, processError);
     };
 
-    this.getView = function(view) {
+    this.getView= function(view) {
         return $http({
-            method : 'POST',
+            method : "POST",
             url : API_URL,
             data: {
                 op:	view
@@ -37,9 +37,9 @@ angular.module('services')
         }).then(processSuccess, processError);
     };
 
-    this.getEvents = function() {
+    this.getEvents= function() {
         return $http({
-            method : 'POST',
+            method : "POST",
             url : API_URL,
             data: {
                 op:	'events'
@@ -49,7 +49,7 @@ angular.module('services')
 
     this.getContactById = function(id) {
         return $http({
-            method : 'POST',
+            method : "POST",
             url : API_URL,
             data: {
                 op:     'id',
@@ -64,15 +64,15 @@ angular.module('services')
      *
      */
     this.getCustomResults = function(customQuery) {
-        var delim= customQuery.indexOf(':');
+        var delim= customQuery.indexOf(':')
 		if (delim == -1) { //name only search
-			var optype= 'name';
-			var queryfield= 'na';
-			var queryitem= customQuery;
+			var optype= 'name'
+			var queryfield= 'na'
+			var queryitem= customQuery
 		} else { //defined search
-			var optype= 'defsrch';
-			var queryfield= customQuery.substr(0, delim);
-			var queryitem = customQuery.substr(delim+1).trim();
+			var optype= 'defsrch'
+			var queryfield= customQuery.substr(0, delim)
+			var queryitem = customQuery.substr(delim+1).trim()
 		}
         return $http({
             method : 'POST',
