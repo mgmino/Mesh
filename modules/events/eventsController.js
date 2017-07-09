0c24034edf9630ba1aa0140c2f284f3af1eb0bf2
mgmino <mmino@sabaitechnology.com>
2017-07-08 22:29:59 -0400

    improve docs, fix events search
    can't get hr logic in events to work

--- modules/events/eventsController.js
+++ modules/events/eventsController.js
@@ -13,6 +13,18 @@ function($scope, $routeParams, queryService, alertService) {
 			});
 	}
 
-	init();
+	$scope.dateline= function (event) {
+		if (looking && mm <= event.substr(5,2) && dd <= event.substr(8,2)) {
+			looking= false
+			console.log(event);
+			return true
+		}
+		return false
+	}
 
+	init();
+	var today= new Date();
+	var dd= today.getDate();
+	var mm= today.getMonth()+1; //January is 0
+	var looking= true
 }]);
