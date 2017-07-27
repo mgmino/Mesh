2d6b70a49e1ab82f1aa4311a6d8c0f03a4757ebd
Unknown <brandonmino@bellsouth.net>
2017-07-27 16:46:11 -0400

    Splitting Filters
    Modular code

--- filters/bornFilter.js
+++ filters/bornFilter.js
@@ -1,13 +1,4 @@
-angular.module('filters', [])
-
-.filter('ageFilter', function() {
-    return function(modDate) {
-        if (!modDate || modDate.substr(0,4) === '0000') return '';
-        var ageDifMs = Date.now() - new Date(modDate);
-        return (ageDifMs /1000 /3600 /24 /365.25).toFixed(1);
-    };
-})
-
+angular.module('filters')
 .filter('bornFilter', function($filter) {
     return function(born) {
         if (!born) return '';
