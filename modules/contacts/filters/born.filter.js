c152827f59c57e73ee0900a0f5ac71817741a52a
Michael Mino <mmino@sabaitechnology.com>
2018-09-16 22:06:35 -0400

    fix age filter

--- modules/contacts/filters/born.filter.js
+++ modules/contacts/filters/born.filter.js
@@ -2,22 +2,22 @@ angular
     .module('contacts')
     .filter('bornFilter', bornFilter);
 
-bornFilter.$inject = [];
+bornFilter.$inject = ['$filter'];
 
 function bornFilter($filter) {
     return function(born) {
         if (!born) return '';
         if (born.substr(0,4) === '0000') {
             if (born.substr(5,2) === '00') return ''; //month and year not known
-            else born = '9999' +born.substr(4);
+            else born = '9999' +born.substr(4); //month known, year not known
         }
-        if (born.substr(5,2) === '00') { //month not known
+        if (born.substr(5,2) === '00') { //year known, month not known
             born = born.substr(0,5) +'07-01';
             var yearOnly= 1;
         }
         var ageDifMs = Date.now() - new Date(born).getTime();
         var ageDate = new Date(ageDifMs); // milliseconds from epoch
-        var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+        var age = Math.abs(ageDate.getUTCFullYear() - 1970); //***fix***
         var birthdate = $filter('date')(born);
         if (age > 200) return birthdate.substr(0,birthdate.indexOf(',', 0));
         else if (typeof(yearOnly) !== 'undefined') return ' [' + age +']';
